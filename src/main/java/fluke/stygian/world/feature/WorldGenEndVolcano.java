ad01896b128125729cc5697e3e7e4852dc230a1c
QUENTIN453 <quentinlamauve@gmail.com>
2023-06-28 15:41:14 +0200

    Revert "Reduce Bottleneck"
    This reverts commit 85885f773010eff113a711da72689a4249f4a60e.

--- src/main/java/fluke/stygian/world/feature/WorldGenEndVolcano.java
+++ src/main/java/fluke/stygian/world/feature/WorldGenEndVolcano.java
@@ -1,10 +1,8 @@
 package fluke.stygian.world.feature;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 import fluke.stygian.util.FastNoise;
 import fluke.stygian.util.FastNoise.NoiseType;
@@ -14,123 +12,124 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
-public class WorldGenEndVolcano extends WorldGenerator {
+public class WorldGenEndVolcano extends WorldGenerator
+{
 	private final IBlockState volcMainState;
 	private final IBlockState volcSecondaryState;
 	private final IBlockState volcLiquid;
 	private final FastNoise perlin;
 	private final static IBlockState AIR = Blocks.AIR.getDefaultState();
-	private final Set<BlockPos> cachedFirstLayerPositions;
-
-	public WorldGenEndVolcano(IBlockState mainBlock, IBlockState subBlock, IBlockState liquid) {
+	
+	public WorldGenEndVolcano(IBlockState mainBlock, IBlockState subBlock, IBlockState liquid)
+	{
 		this.volcMainState = mainBlock;
 		this.volcSecondaryState = subBlock;
 		this.volcLiquid = liquid;
-		this.perlin = new FastNoise();
-		this.perlin.SetNoiseType(NoiseType.Perlin);
-		this.perlin.SetFrequency(0.1F);
-		this.cachedFirstLayerPositions = new HashSet<>();
+		perlin = new FastNoise();
+		perlin.SetNoiseType(NoiseType.Perlin);
+		perlin.SetFrequency(0.1F);
 	}
-
-	public boolean generate(World world, Random rand, BlockPos pos) {
-		int radius = 9 + rand.nextInt(6);
-		int maxHeight = radius * 2 - 1 - rand.nextInt(2);
+	
+	public boolean generate(World world, Random rand, BlockPos pos)
+	{
+		int radius = 9+rand.nextInt(6);
+		int maxHeight = radius*2 - 1 - rand.nextInt(2);
 		IBlockState volcBlock = volcMainState;
 		List<BlockPos> firstLayer = getFirstLayer(world, pos, radius);
-
-		if (firstLayer == null)
+		
+		if(firstLayer == null)
 			return false;
-
-		for (BlockPos baseBlock : firstLayer) {
-			world.setBlockState(baseBlock, volcBlock);
-			cachedFirstLayerPositions.add(baseBlock);
-		}
-
-		for (int y = 1; y < maxHeight; y++) {
-			int layerRadius = radius - y / 2;
-			double maxDist = layerRadius * layerRadius;
-
-			for (int x = -layerRadius - 2; x <= layerRadius + 2; x++) {
-				double xDist = x * x;
-
-				for (int z = -layerRadius - 2; z <= layerRadius + 2; z++) {
-					double zDist = z * z;
-					double noiseMod = (perlin.GetNoise(pos.getX() + x, y * 2, pos.getZ() + z) + 1) / 2.0 + 0.5;
-					double noisyDist = xDist * noiseMod + zDist * noiseMod;
-
-					if (noisyDist > maxDist)
-						continue;
-
-					if (xDist <= 1 && zDist <= 1 && y < 12) {
-						volcBlock = volcLiquid;
-					} else if (rand.nextInt(11) == 0) {
-						volcBlock = volcSecondaryState;
-					} else {
-						volcBlock = volcMainState;
-					}
-
-					BlockPos blockPos = pos.add(x, y, z);
-					IBlockState downState = world.getBlockState(blockPos.down());
-					if (y == 0 || volcBlock == volcLiquid || downState == volcMainState || downState == volcSecondaryState || (y == 12 && (xDist <= 1 && zDist <= 1))) {
-						world.setBlockState(blockPos, volcBlock);
-					}
-				}
+		else
+		{
+			for(BlockPos baseBlock : firstLayer)
+			{
+				world.setBlockState(baseBlock, volcBlock);
 			}
 		}
+		
+		for(int y=1; y < maxHeight; y++)
+		{
+			int layerRadius = radius - y/2;
+			double maxDist = layerRadius*layerRadius;
+			
+			for(int x=-layerRadius-2; x<=layerRadius+2; x++)
+			{
+				double xDist = x*x;
+				
+				for(int z=-layerRadius-2; z<=layerRadius+2; z++)
+				{
+					double zDist = z*z;
+					double noiseMod = (perlin.GetNoise(pos.getX()+x, y*2, pos.getZ()+z)+1)/2.0 + 0.5;
+					double noisyDist = xDist*noiseMod + zDist*noiseMod;
+					
+					if(noisyDist > maxDist)
+						continue;
+					
+					if(x == 0 && z == 0)
+						volcBlock = volcLiquid;
+					else if(y < 12 && xDist <= 1 && zDist <= 1)
+						volcBlock = volcLiquid;
+					else if(rand.nextInt(11) == 0)
+						volcBlock = volcSecondaryState;
+					else
+						volcBlock = volcMainState;
+					
+					IBlockState downState = world.getBlockState(pos.add(x, y, z).down());
+					if(y == 0 ||  volcBlock == volcLiquid || downState == volcMainState || downState == volcSecondaryState || (y==12 && (xDist <= 1 && zDist <= 1)))
+						world.setBlockState(pos.add(x, y, z), volcBlock);
+					
+				}
+			}
+		}		
 		return true;
 	}
-
-	// Returns a list of block positions for the first layer of the volcano, may be up to -2 lower than the starting pos if the terrain lowers
-	// Returns null if the terrain is not flat enough for the volcano
-	public List<BlockPos> getFirstLayer(World world, BlockPos pos, int radius) {
-		if (!cachedFirstLayerPositions.isEmpty()) {
-			List<BlockPos> firstLayer = new ArrayList<>();
-			for (BlockPos cachedPos : cachedFirstLayerPositions) {
-				firstLayer.add(pos.add(cachedPos));
-			}
-			return firstLayer;
-		}
-
-		List<BlockPos> firstLayer = new ArrayList<>();
-		double maxDist = radius * radius;
-
-		for (int x = -radius - 2; x <= radius + 2; x++) {
-			double xDist = x * x;
-
-			for (int z = -radius - 2; z <= radius + 2; z++) {
-				double zDist = z * z;
-				double noiseMod = (perlin.GetNoise(pos.getX() + x, 0, pos.getZ() + z) + 1) / 2.0 + 0.5;
-				double noisyDist = xDist * noiseMod + zDist * noiseMod;
-
-				if (noisyDist > maxDist)
+	
+	//returns a list of block pos for first layer of volcano, may be up to -2 lower than starting pos if terrain lowers
+	//will return null if terrain is not flat enough for volcano
+	public List<BlockPos> getFirstLayer(World world, BlockPos pos, int radius)
+	{
+		List<BlockPos> firstLayer = new ArrayList<BlockPos>();
+		double maxDist = radius*radius;
+		
+		for(int x=-radius-2; x<=radius+2; x++)
+		{
+			double xDist = x*x;
+			
+			for(int z=-radius-2; z<=radius+2; z++)
+			{
+				double zDist = z*z;
+				double noiseMod = (perlin.GetNoise(pos.getX()+x, 0, pos.getZ()+z)+1)/2.0 + 0.5;
+				double noisyDist = xDist*noiseMod + zDist*noiseMod;
+				
+				if(noisyDist > maxDist)
 					continue;
-
+				
 				BlockPos checkPos = pos.add(x, 0, z).down();
 				IBlockState downState = world.getBlockState(checkPos);
-				if (downState != AIR) {
-					BlockPos blockPos = pos.add(x, 0, z);
-					firstLayer.add(blockPos);
-					cachedFirstLayerPositions.add(blockPos.subtract(pos));
-				} else if (world.getBlockState(checkPos.down()) != AIR) {
-					BlockPos blockPos = pos.add(x, 0, z);
-					firstLayer.add(blockPos);
-					firstLayer.add(blockPos.down());
-					cachedFirstLayerPositions.add(blockPos.subtract(pos));
-					cachedFirstLayerPositions.add(blockPos.subtract(pos).down());
-				} else if (world.getBlockState(checkPos.down().down()) != AIR) {
-					BlockPos blockPos = pos.add(x, 0, z);
-					firstLayer.add(blockPos);
-					firstLayer.add(blockPos.down());
-					firstLayer.add(blockPos.down(2));
-					cachedFirstLayerPositions.add(blockPos.subtract(pos));
-					cachedFirstLayerPositions.add(blockPos.subtract(pos).down());
-					cachedFirstLayerPositions.add(blockPos.subtract(pos).down(2));
-				} else {
+				if(downState != AIR)
+				{
+					firstLayer.add(pos.add(x, 0, z));
+				}
+				else if(world.getBlockState(checkPos.down()) != AIR)
+				{
+					firstLayer.add(pos.add(x, 0, z));
+					firstLayer.add(pos.add(x, -1, z));
+				}
+				else if(world.getBlockState(checkPos.down().down()) != AIR)
+				{
+					firstLayer.add(pos.add(x, 0, z));
+					firstLayer.add(pos.add(x, -1, z));
+					firstLayer.add(pos.add(x, -2, z));
+				}
+				else
+				{
 					return null;
 				}
+				
 			}
 		}
-
+		
 		return firstLayer;
 	}
+
 }
